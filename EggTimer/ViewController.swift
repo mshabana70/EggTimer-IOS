84a6877096d9d40d78ef05fb8ca0ae0732ad263a
Mahmoud Shabana <mshabana5656@gmail.com>
2020-06-10 06:32:18 -0400

    Update UI and implement progressBar

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -11,6 +11,83 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    /* Rather than use 3 constants that hold one piece of data
+    let softTime = 5
+    let mediumTime = 8
+    let hardTime = 12
+    */
+    
+    // We can create a dictionary to hold the three values for our egg timer
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    
+    var secondsRemaining = 60
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    @IBOutlet weak var eggLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        // The output of eggTimes[hardness] is an optional (Int?), must unwrap in print statement
+        //print(eggTimes[hardness]!)
+        
+        /* Checking values with IF/ELSE
+        if hardness == "Soft" {
+            print(softTime)
+        } else if hardness == "Medium" {
+            print(mediumTime)
+        } else if hardness == "Hard" {
+            print(hardTime)
+        } else {
+            print("Error in If statement")
+        }*/
+        
+        /*Checking values with Switch Case
+        switch hardness {
+        case "Soft":
+            print(softTime)
+        case "Medium":
+            print(mediumTime)
+        case "Hard":
+            print(hardTime)
+        default:
+            print("Error in If statement")
+        }*/
+        
+        //If a new button is pressed, then the timer is reset to the value that secondsRemaining has changed to
+        //without speeding up the countdown.
+        timer.invalidate()
+        
+        
+        let hardness = sender.currentTitle! // Soft, Medium, Hard
+        
+        // setting the starting time to the seconds that is paired with key "hardness" returns
+        totalTime = eggTimes[hardness]!
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        eggLabel.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        }
+    
+        @objc func updateCounter() {
+            if secondsPassed < totalTime {
+                print("\(secondsPassed) seconds.")
+                
+                secondsPassed += 1
+                progressBar.progress = Float(secondsPassed) / Float(totalTime)
+                //print(Float(secondsPassed) / Float(totalTime))
+                //print(Float(secondsPassed / totalTime))
+            } else {
+                timer.invalidate()
+                self.eggLabel.text = "DONE!"
+            }
+        }
+    
+        
+    }
+    
 
 
-}
