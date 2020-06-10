13d7f0685d99dc28097eda86748c4a9f560e8f96
Mahmoud Shabana <mshabana5656@gmail.com>
2020-06-10 06:40:43 -0400

    Adding Alarm sound using AVAudioPlayer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -8,6 +8,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
@@ -24,6 +25,8 @@ class ViewController: UIViewController {
     var timer = Timer()
     var totalTime = 0
     var secondsPassed = 0
+    
+    var player: AVAudioPlayer!
     
     @IBOutlet weak var eggLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
@@ -83,6 +86,9 @@ class ViewController: UIViewController {
             } else {
                 timer.invalidate()
                 self.eggLabel.text = "DONE!"
+                let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+                player = try! AVAudioPlayer(contentsOf: url!)
+                player.play()
             }
         }
     
